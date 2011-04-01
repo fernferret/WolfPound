c7961da542256e65248c1a8ffce0aca5bad7f26c
Eric Stokes <fernferret@gmail.com>
2011-03-31 21:31:57 -0400

    Initial Commit

---
+++ src/com/fernferret/wolfpound/WolfPound.java
@@ -0,0 +1,19 @@
+package com.fernferret.wolfpound;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WolfPound extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}

ebbdb199fa273656c44f094c617797e7f667148b
Eric Stokes <fernferret@gmail.com>
2011-04-02 23:24:12 -0400

    Fix econ with items

--- src/com/fernferret/wolfpound/WPPlayerListener.java
+++ src/com/fernferret/wolfpound/WPPlayerListener.java
@@ -1,10 +1,12 @@
 package com.fernferret.wolfpound;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.craftbukkit.block.CraftSign;
 
@@ -21,8 +23,15 @@ public class WPPlayerListener extends PlayerListener {
 		if (event.hasBlock() && event.getClickedBlock().getState() instanceof Sign && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			if (plugin.blockIsValidWolfSign(event.getClickedBlock()) && plugin.hasPermission(p, WolfPound.PERM_USE)) {
 				// We have a valid pound!
-				double price = getPriceFromBlock(event.getClickedBlock(), 1);
-				int item = getItemFromBlock(event.getClickedBlock(), 1);
+				double price = 0;
+				int item = -1;
+				if(checkSignParms(event.getClickedBlock(), 1) > 0) {
+					price = getPriceFromBlock(event.getClickedBlock(), 1);
+				} 
+				if(checkSignParms(event.getClickedBlock(), 1) > 1){
+					item = getItemFromBlock(event.getClickedBlock(), 1);
+				}
+				
 				if (plugin.bank.hasMoney(p, price, item)) {
 					plugin.bank.payForWolf(p, price, item);
 					plugin.bank.showRecipt(p, price, item);
@@ -30,6 +39,13 @@ public class WPPlayerListener extends PlayerListener {
 				}
 			}
 		}
+	}
+	
+	private int checkSignParms(Block b, int l) {
+		Sign s = new CraftSign(b);
+		String line = s.getLine(l);
+		String[] items = line.split(":");
+		return items.length;
 	}
 
 	/**
@@ -44,29 +60,42 @@ public class WPPlayerListener extends PlayerListener {
 			if (b.getState() instanceof Sign) {
 				Sign s = new CraftSign(b);
 				String line = s.getLine(l);
-				String[] items = line.split("(.*):(.*)");
-				if(items.length > 0) {
+				String[] items = line.split(":");
+				if (items.length > 0) {
 					return Double.parseDouble(items[0].replaceAll("\\D", ""));
 				}
 			}
 		} catch (NumberFormatException e) {
 			// We'll return the default
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// Again, we'll return the default
 		}
 		return 0.0;
 	}
 	
 	private int getItemFromBlock(Block b, int l) {
-		try {
-			if (b.getState() instanceof Sign) {
+		String line = "";
+		String[] items;
+		
+		if (b.getState() instanceof Sign) {
+			try {
 				Sign s = new CraftSign(b);
-				String line = s.getLine(l);
-				String[] items = line.split(":");
-				if(items.length > 0) {
+				line = s.getLine(l);
+				items = line.split(":");
+				if (items.length > 0) {
 					return Integer.parseInt(items[1].replaceAll("\\D", ""));
 				}
+			} catch (NumberFormatException e) {
+				Sign s = new CraftSign(b);
+				line = s.getLine(l);
+				items = line.split(":");
+				if (items.length > 0) {
+					Material m = Material.matchMaterial(items[1]);
+					return m.getId();
+				}
+			} catch (ArrayIndexOutOfBoundsException e) {
+				
 			}
-		} catch (NumberFormatException e) {
-			// We'll return the default
 		}
 		return -1;
 	}

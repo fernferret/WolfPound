9b1e4c876ec7f139e356702a8f499b2711160a79
Eric Stokes <fernferret@gmail.com>
2011-05-05 01:12:13 -0400

    Cleanup CommandPrice

--- src/com/fernferret/wolfpound/commands/CommandPrice.java
+++ src/com/fernferret/wolfpound/commands/CommandPrice.java
@@ -7,38 +7,46 @@ import org.bukkit.entity.Player;
 import com.fernferret.wolfpound.WolfPound;
 
 public class CommandPrice extends WolfPoundCommand {
-	private static final String[] PRICE_KEYWORDS = {"all"};
+	private static final String[] PRICE_KEYWORDS = { "all" };
+	
 	public CommandPrice(WolfPound plugin) {
 		super(plugin);
 	}
-
+	// TODO: place price messengers in here!
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		// Someone tried just /wpprice
-		if(args.length == 0) {
-			if(sender instanceof Player) {
-				Player p = (Player)sender;
+		Player p = null;
+		if (sender instanceof Player) {
+			p = (Player) sender;
+		}
+		if (args.length == 0) {
+			if (p != null) {
 				plugin.sendWolfPrice(p, p.getWorld().getName());
 				return true;
 			} else {
-				WolfPound.log.info("Please use: /wpprice {w:WORLD|all} from the console");
-			}
-		} else if(args.length == 1) {
-			if(checkKeyword(args[0], PRICE_KEYWORDS) == "all") {
-				if(sender instanceof Player) {
-					this.plugin.sendWolfPrice((Player)sender, "all");
-					return true;
-				}
-				// TODO: Fix for console
-			} else if(isValidWorld(args[0])) {
-				this.plugin.sendWolfPrice((Player)sender, getWorldName(args[0]));
+				WolfPound.log.info("Please use: wpprice [w:WORLD | all]");
 				return true;
-				// TODO: Fix for console
-			} else {
-				if(!(sender instanceof Player)) {
-					WolfPound.log.info("Please use: /wpprice {w:WORLD|all}");
-				}
 			}
+		} else if (args.length == 1) {
+			if (checkKeyword(args[0], PRICE_KEYWORDS) == "all") {
+				if (p != null) {
+					this.plugin.sendWolfPrice((Player) sender, "all");
+				} else {
+					// TODO: Add console support
+					WolfPound.log.info("This command is currently not supported from the console.");
+				}
+			} else if (isValidWorld(args[0])) {
+				if (p != null) {
+					this.plugin.sendWolfPrice((Player) sender, getWorldName(args[0]));
+				} else {
+					// TODO: Add console support
+					WolfPound.log.info("This command is currently not supported from the console.");
+				}
+			} else if (!(sender instanceof Player)) {
+				WolfPound.log.info("Please use: wpprice [w:WORLD | all]");
+			}
+			return true;
 		}
 		return false;
 	}

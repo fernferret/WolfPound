0a579912beeabb5c1eafc17554a3d26715f212e3
Eric Stokes <fernferret@gmail.com>
2011-06-01 14:25:20 -0400

    Removed extra console spam, should be good now

--- src/com/fernferret/wolfpound/WPPluginListener.java
+++ src/com/fernferret/wolfpound/WPPluginListener.java
@@ -1,9 +1,5 @@
 package com.fernferret.wolfpound;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.logging.Logger;
-
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.event.server.ServerListener;
 
@@ -11,9 +7,6 @@ import com.fernferret.allpay.AllPay;
 
 public class WPPluginListener extends ServerListener {
 	private WolfPound plugin;
-	String elements[] = { "iConomy", "BOSEconomy", "Essentials", "RealBank" };
-	HashSet<String> set = new HashSet<String>(Arrays.asList(elements));
-	public static final Logger log = Logger.getLogger("Minecraft");
 	
 	public WPPluginListener(WolfPound plugin) {
 		this.plugin = plugin;
@@ -22,12 +15,9 @@ public class WPPluginListener extends ServerListener {
 	@Override
 	public void onPluginEnable(PluginEnableEvent event) {
 		// Let AllPay handle all econ plugin loadings, only go for econ plugins we support
-		
 		if (event.getPlugin().getDescription().getName().equals("WolfPound")) {
 			AllPay banker = new AllPay(plugin, WolfPound.logPrefix + " ");
 			plugin.bank = banker.getEconPlugin();
-		} else {
-			log.warning("ALLPAY - Did not find an econ plugin this time: " + event.getPlugin().getDescription().getName());
 		}
 	}
 }
